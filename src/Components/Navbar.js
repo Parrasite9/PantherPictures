d7157c7407160f3ad6435ebacb698bb3b672df7b
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-03-01 19:43:01 -0600

    Navbar created, not properly CSSd at this time. Will continue the CSS at a later time.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../CSS/Navbar.css'
+
 
 function Navbar() {
   return (
     <div className='navbar'>
-      
+        <div className="navbar__container">
+              <div className='navbar__left'>
+                  <a className='logo' href='#'>
+                      <img src="/Images/Logo.png" alt="" />
+                  </a>
+              </div>
+
+              <div className='navbar__right'>
+                  <div className="navbar__link"><a href=''>HOME</a></div>
+                  <div className="navbar__link"><a href=''>NOW SHOWING</a></div>
+                  {/* <div className="navbar__link"><a href=''>JOIN THE TEAM</a></div> */}
+                  <div className="navbar__link"><a href=''>ADVERTISEMENT</a></div>
+                  <div className="navbar__link"><a href=''>THEATER RENTAL</a></div>
+              </div>
+        </div>
     </div>
   )
 }
