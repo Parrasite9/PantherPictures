17a56ff7a18864cb7ceac9c578150b684e0e2460
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-03-02 16:42:43 -0600

    attempted to add a google map, was unsuccessful. Will return at a later time to make another attempt. Google map react package was installed here

--- README.md
+++ README.md
@@ -75,4 +75,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 ## Installed Packages
 - react slick (npm install react-slick --save)
 - react slick css (npm install slick-carousel --save)
+- google maps (npm install --save google-map-react)
 
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import googleMapReact from 'google-map-react'
 
 function Map() {
-  return (
-    <div className='map'>
-      
-    </div>
-  )
-}
 
+  const MapMarker = ({text}) => <div>{text}</div>
+
+  function Map() {
+    const defaultProps = {
+      center: {
+        lat: 10.00935602,
+        lng: 77.01502627,
+      },
+      zoom: 11,
+    }
+
+    return (
+      <div className='map'>
+        <googleMapReact 
+            bootstrapURLKeys={{ key: ''}} 
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+            >
+          <MapMarker lat={59.955413} lng={30.337844} text='My Marker' />
+        </googleMapReact>
+        
+      </div>
+    )
+  }
+
+
+}
 export default Map
+
+// AIzaSyDJOfK6PcbasoGz2P45EYp23HC3FwvnzSw
