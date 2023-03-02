b48b1020e454505445fe12a76a88f61cfe5d39bc
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-03-02 14:55:15 -0600

    Hero section added and is currently CSSd properly.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -24,24 +24,32 @@ function Hero(props) {
           <div className='slide__Item'>
               <a href="">
                   <img src="/Images/antman-slider.png" alt="" className="slider__Img" />
+                  <button>Get Tickets</button>
+                  <button>Learn More</button>
               </a>
           </div>
 
           <div className='slide__Item'>
               <a href="">
                   <img src="/Images/magicmike-slider.png" alt="" className="slider__Img" />
+                  <button>Get Tickets</button>
+                  <button>Learn More</button>
               </a>
           </div>
 
           <div className='slide__Item'>
               <a href="">
                   <img src="/Images/creed-slider.png" alt="" className="slider__Img" />
+                  <button>Get Tickets</button>
+                  <button>Learn More</button>
               </a>
           </div>
 
           <div className='slide__Item'>
               <a href="">
                   <img src="/Images/jesus-slider.png" alt="" className="slider__Img" />
+                  <button>Get Tickets</button>
+                  <button className='hide__button'>Learn More</button>
               </a>
           </div>
 
